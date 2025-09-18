f2ea9cbfaec86d5789a2deb2a689eca4e6671bf4
bachdnd <bachbeo200003@gmail.com>
2025-09-19 00:16:01 +0700

    add(ui): create AI Chat frontend interface

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,35 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
-import { AnimatePresence } from 'framer-motion';
-import './index.css';
-import '@fontsource/montserrat';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { SignedIn, SignedOut } from '@clerk/clerk-react';
-import ProtectedRoute from './components/ProtectedRoute';
+import React from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import "./index.css";
+import "@fontsource/montserrat";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { SignedIn, SignedOut } from "@clerk/clerk-react";
+import ProtectedRoute from "./components/ProtectedRoute";
 
-import LandingPage from './page/user/LandingPage';
-import LoginPage from './page/user/LoginPage';
-import SignupPage from './page/user/SignupPage';
-import BusinessPage from './page/user/BusinessPage';
-import UserProfilePage from './page/user/UserProfilePage';
-import AuthCallback from './auth/AuthCallback';
-import PersonalizedPage from './page/user/PersonalizedPage';
-import DiscoverPage from './page/user/DiscoverPage';
-import DiscoverByCategoryPage from './page/user/DiscoverByCategoryPage';
-import AnimatedLayout from './components/AnimatedLayout';
-import MyBusinessPage from './page/user/MyBusinessPage';
-import ProductRegistrationPage from './page/user/ProductRegistrationPage';
-import BusinessRegistrationPage from './page/user/BusinessRegistrationPage';
-import ManageUserPage from './page/admin/ManageUserPage';
-import ManageBusinessPage from './page/admin/ManageBusinessPage';
-import ManageTransactionPage from './page/admin/ManageTransactionPage';
-import UserPayComplete from './components/UserPayComplete';
-import StackPage from './page/user/StackPage';
-import AdminRoute from './components/AdminRoute';
-import OwnerRoute from './components/OwnerRoute';
-import ClientRoute from './components/ClientRoute';
+import LandingPage from "./page/user/LandingPage";
+import LoginPage from "./page/user/LoginPage";
+import SignupPage from "./page/user/SignupPage";
+import BusinessPage from "./page/user/BusinessPage";
+import UserProfilePage from "./page/user/UserProfilePage";
+import AuthCallback from "./auth/AuthCallback";
+import PersonalizedPage from "./page/user/PersonalizedPage";
+import DiscoverPage from "./page/user/DiscoverPage";
+import DiscoverByCategoryPage from "./page/user/DiscoverByCategoryPage";
+import AnimatedLayout from "./components/AnimatedLayout";
+import MyBusinessPage from "./page/user/MyBusinessPage";
+import ProductRegistrationPage from "./page/user/ProductRegistrationPage";
+import BusinessRegistrationPage from "./page/user/BusinessRegistrationPage";
+import ManageUserPage from "./page/admin/ManageUserPage";
+import ManageBusinessPage from "./page/admin/ManageBusinessPage";
+import ManageTransactionPage from "./page/admin/ManageTransactionPage";
+import UserPayComplete from "./components/UserPayComplete";
+import StackPage from "./page/user/StackPage";
+import AdminRoute from "./components/AdminRoute";
+import OwnerRoute from "./components/OwnerRoute";
+import ClientRoute from "./components/ClientRoute";
+import AiChatLayout from "./layout/AiChatLayout.jsx";
 
 const AppRoutes = () => {
   const location = useLocation();
@@ -39,6 +40,8 @@ const AppRoutes = () => {
         {/* Public routes */}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/ai" element={<AiChatLayout />} />
+
         <Route
           path="/auth-callback"
           element={
@@ -70,10 +73,46 @@ const AppRoutes = () => {
             path="/discover/:category"
             element={<DiscoverByCategoryPage />}
           />
-          <Route path="/user-profile/*" element={<><SignedIn><UserProfilePage /></SignedIn><SignedOut><LoginPage /></SignedOut></>} />
+          <Route
+            path="/user-profile/*"
+            element={
+              <>
+                <SignedIn>
+                  <UserProfilePage />
+                </SignedIn>
+                <SignedOut>
+                  <LoginPage />
+                </SignedOut>
+              </>
+            }
+          />
 
-          <Route path="/payment-complete" element={<><SignedIn><UserPayComplete /></SignedIn><SignedOut><LoginPage /></SignedOut></>} />
-          <Route path="/stacks" element={<><SignedIn><StackPage /></SignedIn><SignedOut><LoginPage /></SignedOut></>} />
+          <Route
+            path="/payment-complete"
+            element={
+              <>
+                <SignedIn>
+                  <UserPayComplete />
+                </SignedIn>
+                <SignedOut>
+                  <LoginPage />
+                </SignedOut>
+              </>
+            }
+          />
+          <Route
+            path="/stacks"
+            element={
+              <>
+                <SignedIn>
+                  <StackPage />
+                </SignedIn>
+                <SignedOut>
+                  <LoginPage />
+                </SignedOut>
+              </>
+            }
+          />
           <Route
             path="/business-registration"
             element={
